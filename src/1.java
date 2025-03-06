e8e7fa79a6985b8799e5acc11e52200186451a0a
makkimkhalid545 <makkimkhalid545@gmail.com>
2025-03-06 09:12:26 +0000

    Add 1.java

---
+++ src/1.java
@@ -0,0 +1,11 @@
+public class RandomJavaCode {
+  public static void main(String[] args) {
+    System.out.println("This is a random Java program.");
+    int x = (int)(Math.random() * 10);
+    if (x % 2 == 0) {
+      System.out.println("Even number");
+    } else {
+      System.out.println("Odd number");
+    }
+  }
+}
