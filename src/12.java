c17313a72c3bfd5c9855473faab800826310feec
makkimkhalid545 <makkimkhalid545@gmail.com>
2025-03-17 06:15:33 +0000

    Add 12.java

---
+++ src/12.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class MySourceCode {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String name;
+        System.out.print("Enter your name: ");
+        name = sc.nextLine();
+        System.out.println("Hello, " + name);
+    }
+}
