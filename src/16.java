81cdf857ac71ecb9bc1595070f2030b09d8994ff
makkimkhalid545 <makkimkhalid545@gmail.com>
2025-03-22 16:20:54 +0000

    Add 16.java

---
+++ src/16.java
@@ -0,0 +1,8 @@
+public class RandomJavaCode {
+    public static void main(String[] args) {
+        // Example of random number generation using the Java Math.random() method
+        int randomNumber = Math.random();
+        
+        System.out.println("Random Number: " + randomNumber);
+    }
+}
