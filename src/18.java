e3f4fc7b4fb2e87dad0ce73184a152d8495cfa6e
makkimkhalid545 <makkimkhalid545@gmail.com>
2025-03-24 03:04:34 +0000

    Add 18.java

---
+++ src/18.java
@@ -0,0 +1,8 @@
+public class FinalProject {
+    public static void main(String[] args) {
+        // Example of using an array to store numbers
+        int[] numbers = {10, 20, 30, 40};
+        
+        System.out.println("Array elements: " + Arrays.toString(numbers));
+    }
+}
