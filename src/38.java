f177e364c9902e5cbb7a11bf350910a61c3ad96c
makkimkhalid545 <makkimkhalid545@gmail.com>
2025-05-17 12:17:20 +0000

    Add 38.java

---
+++ src/38.java
@@ -0,0 +1,6 @@
+public class FinalProject {
+    public static void main(String[] args) {
+        // Example of a Java program that prints "Hello, World!" to the console.
+        System.out.println("Hello, World!");
+    }
+}
